2c24c0072e6cf1da4b3292bda18fd333a500de47
lucas_hub12 <hyeonggyunbae@gmail.com>
2025-04-08 17:29:03 +0900

    wiki dag

--- pyspark/movie_meta.py
+++ pyspark/movie_meta.py
@@ -1,97 +1,66 @@
-from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
 import sys
 import logging
 
 logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    # handlers=[logging.StreamHandler(sys.stdout)]  # stdout으로 출력
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)] # stdout으로 출력
 )
-
 spark = SparkSession.builder.appName("movie_meta").getOrCreate()
 
-exit_code = 0 # O 이면 정상 종료, 1 이면 비정상 종료
+exit_code = 0 # 0 이면 정상 종료, 1 이면 비정상 종료
 
 def save_meta(df: DataFrame, meta_path: str):
     df.write.mode("overwrite").parquet(meta_path)
-    logging.info(f"메타 저장 완료 : {meta_path}")
+    logging.info(f"meta 저장 완료: {meta_path}")
 
-try:    
+try:
     if len(sys.argv) != 4:
-        raise ValueError("필수 인자가 누락되었습니다")
-    
+        raise ValueError("필수 인자가 누락되었습니다.")
     raw_path, mode, meta_path = sys.argv[1:4]
     
     meta_today = spark.read.parquet(raw_path).select("movieCd","multiMovieYn", "repNationCd")
     
     if mode == "create":
-        spark.read.parquet(raw_path).createOrReplaceTempView("temp_meta_today")
-        meta_today = spark.sql("""
-            WITH cleaned AS (
-            SELECT
-                movieCd,
-                NULLIF(multiMovieYn, 'Unclassified') AS multiMovieYn,
-                NULLIF(repNationCd, 'Unclassified') AS repNationCd
-            FROM temp_meta_today
-        ),
-        grouped AS (
-            SELECT
-                movieCd,
-                COALESCE(MAX(multiMovieYn)) AS multiMovieYn,
-                COALESCE(MAX(repNationCd)) AS repNationCd
-            FROM cleaned
-            GROUP BY movieCd
-        )
-        SELECT * FROM grouped
-        """)
         save_meta(meta_today, meta_path)
-    
     elif mode == "append":
         meta_yesterday = spark.read.parquet(meta_path)
         meta_yesterday.createOrReplaceTempView("temp_meta_yesterday")
-
-       
-        spark.read.parquet(raw_path).createOrReplaceTempView("raw_meta_today")
+        meta_today.createOrReplaceTempView("temp_meta_today")
         
-        spark.sql("""
-            CREATE OR REPLACE TEMP VIEW temp_meta_today AS
-            WITH cleaned AS (
-                SELECT
-                    movieCd,
-                    NULLIF(multiMovieYn, 'Unclassified') AS multiMovieYn,
-                    NULLIF(repNationCd, 'Unclassified') AS repNationCd
-                FROM raw_meta_today
-            ),
-            grouped AS (
-                SELECT
-                    movieCd,
-                    COALESCE(MAX(multiMovieYn)) AS multiMovieYn,
-                    COALESCE(MAX(repNationCd)) AS repNationCd
-                FROM cleaned
-                GROUP BY movieCd
-            )
-            SELECT * FROM grouped
-            """)
-    
         updated_meta = spark.sql("""
             SELECT 
-            COALESCE(y.movieCd, t.movieCd) AS movieCd,
-            COALESCE(y.multiMovieYn, t.multiMovieYn) AS multiMovieYn,
-            COALESCE(y.repNationCd, t.repNationCd) AS repNationCd
-            FROM temp_meta_yesterday y
-            FULL OUTER JOIN temp_meta_today t
-            ON y.movieCd = t.movieCd
-            """)
+                movieCd,
+                MAX(multiMovieYn) AS multiMovieYn,
+                MAX(repNationCd) AS repNationCd
+            FROM (
+                SELECT 
+                    COALESCE(y.movieCd, t.movieCd) AS movieCd,
+                    COALESCE(y.multiMovieYn, t.multiMovieYn) AS multiMovieYn,
+                    COALESCE(y.repNationCd, t.repNationCd) AS repNationCd
+                FROM temp_meta_yesterday y
+                FULL OUTER JOIN temp_meta_today t
+                ON y.movieCd = t.movieCd
+            )
+            GROUP BY movieCd
+        """)
         updated_meta.createOrReplaceTempView("temp_meta_update")
         
         # ASSERT_TRUE - updated meta 는 항상 원천 소스 보다 크면 정산 / else 실패
         spark.sql("""
         SELECT ASSERT_TRUE(
-                COALESCE((SELECT COUNT(*) FROM temp_meta_update), 0) >= COALESCE((SELECT COUNT(*) FROM temp_meta_yesterday), 0)
-                AND
-                COALESCE((SELECT COUNT(*) FROM temp_meta_update), 0) >= COALESCE((SELECT COUNT(*) FROM temp_meta_today), 0)
-                ) AS is_valid
+            (SELECT COUNT(*) FROM temp_meta_update) > (SELECT COUNT(*) FROM temp_meta_yesterday) AND
+            (SELECT COUNT(*) FROM temp_meta_update) > (SELECT COUNT(*) FROM temp_meta_today)
+        ) AS is_valid
+        """)
         
+        spark.sql("""
+        SELECT ASSERT_TRUE(
+            (SELECT COUNT(DISTINCT movieCd) FROM temp_meta_update) == (SELECT COUNT(*) FROM temp_meta_update)
+        ) AS is_unique
+    
         /*
         WITH counts AS (
             SELECT 
@@ -100,7 +69,7 @@ try:
                 (SELECT COUNT(*) FROM temp_meta_today) AS count_today
         )
         SELECT ASSERT_TRUE(
-            count_update >= count_yesterday AND count_update >= count_today
+            count_update > count_yesterday AND count_update > count_today
         ) AS is_valid
         FROM counts
         */
@@ -109,10 +78,9 @@ try:
         save_meta(updated_meta, meta_path)
     else:
         raise ValueError(f"알 수 없는 MODE: {mode}")
-        
-    
+
 except Exception as e:
-    logging.error(f"오류 : {str(e)}")
+    logging.error(f"오류: {str(e)}")
     exit_code = 1
 finally:
     spark.stop()
